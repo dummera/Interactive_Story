79e0547ebd360047288f5e28183d303365d7c89e
Andrew Dummer <dummera@onid.oregonstate.edu>
2015-07-05 20:08:29 -0700

    Initial commit

---
+++ app/src/main/java/come/example/andrew/interactivestory/Model/Choice.java
@@ -0,0 +1,30 @@
+package come.example.andrew.interactivestory.Model;
+
+/**
+ * Created by Andrew on 5/4/2015.
+ */
+public class Choice {
+    private String mText;
+    private int mNextPage;
+
+    public Choice(String text, int nextPage){
+        mText = text;
+        mNextPage = nextPage;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public int getNextPage() {
+        return mNextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        mNextPage = nextPage;
+    }
+}
---
+++ app/src/main/java/come/example/andrew/interactivestory/UI/StoryActivity.java
@@ -0,0 +1,100 @@
+package come.example.andrew.interactivestory.UI;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import come.example.andrew.interactivestory.Model.Page;
+import come.example.andrew.interactivestory.Model.Story;
+import come.example.andrew.interactivestory.R;
+
+public class StoryActivity extends Activity {
+
+
+    public static final String TAG = StoryActivity.class.getSimpleName();
+
+    private Story mStory = new Story();
+    private ImageView mImageView;
+    private TextView mTextView;
+    private Button mChoice1;
+    private Button mChoice2;
+    private String mName;
+    private Page mCurrentPage;
+
+    public StoryActivity() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+        Intent intent = getIntent();
+        String mName = intent.getStringExtra(getString(R.string.key_name));
+
+        if (mName == null) {
+            mName = "FREDDY";
+        }
+        Log.d(TAG, mName);
+
+        mImageView = (ImageView)findViewById(R.id.storyImageView);
+        mTextView = (TextView)findViewById(R.id.storyTextView);
+        mChoice1 = (Button)findViewById(R.id.choiceButton1);
+        mChoice2 = (Button)findViewById(R.id.choiceButton2);
+
+        loadPage(0);
+    }
+
+    private void loadPage(int choice) {
+        mCurrentPage = mStory.getPage(choice);
+        Drawable drawable = getResources().getDrawable(mCurrentPage.getImageId());
+        mImageView.setImageDrawable(drawable);
+        String pageText = mCurrentPage.getText();
+        //Add the name if placeholder included.
+        pageText = String.format(pageText, mName);
+
+        if (mCurrentPage.isFinal()) {
+            //Toast("Hey");
+            mChoice1.setVisibility(View.INVISIBLE);
+            mChoice2.setText("Play Again");
+            mChoice2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    finish();
+                }
+            });
+        } else {
+            mTextView.setText(mCurrentPage.getText());
+
+            mChoice1.setText(mCurrentPage.getChoice1().getText());
+            mChoice2.setText(mCurrentPage.getChoice2().getText());
+
+            mChoice1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int nextPage = mCurrentPage.getChoice1().getNextPage();
+                    loadPage(nextPage);
+                }
+
+
+            });
+
+            mChoice2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int nextPage = mCurrentPage.getChoice2().getNextPage();
+                    loadPage(nextPage);
+                }
+
+
+            });
+        }
+    }
+}
